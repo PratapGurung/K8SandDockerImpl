1643cd7dd4e6fc512493996d17b4e4f93317c068
unknown <pratapgurung2071@gmail.com>
2022-01-27 23:17:19 -0600

    adder docker file

--- src/main/java/com/goreto/K8SandDockerImpl/AccountApplication.java
+++ src/main/java/com/goreto/K8SandDockerImpl/AccountApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RefreshScope
 public class AccountApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
-
 	}
 
 }
